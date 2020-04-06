f5b0abcf17db907c213c4bce0e5d869d6e5c81c2
Nabiya-fatima <n.f.quddus@gmail.com>
2020-04-06 10:47:56 +0500

    Assignment1

---
+++ QuadraticEquation/src/quadraticequation/QuadraticEquation.java
@@ -0,0 +1,237 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadraticequation;
+
+import static java.lang.Byte.MAX_VALUE;
+import java.util.*;
+import java.lang.*;
+/**
+ *
+ * @author Nabiya Fatima
+ */
+public class QuadraticEquation {
+
+    int a;
+    int b;
+    int c;
+    int disc;
+    public void setValues(int x,int y,int z)
+    {
+        this.a=x;
+        this.b=y;
+        this.c=z;
+    }
+    public String CheckRoots()
+    {
+       if(isQuadratic())
+       {
+        disc=calculateDisc();
+        if(disc>0)
+        {
+            return "real";
+        }
+        else if(disc==0)
+        {
+            return "equal";
+        }
+        else if (disc<0)
+        {
+            return "imaginary";
+        }
+       }
+        return null;
+    }
+    public double RootsSum()
+    {
+        double Sum=0;
+        if(isQuadratic())
+        {
+            Sum=(-b)/a;
+        }
+      
+        
+        return Sum;
+    }
+    public int RootProduct()
+    {
+        int product=0;
+        if(isQuadratic())
+        {
+             product=c/a;
+        }
+       
+      
+       
+        return product;
+    }
+    public double[] calculateRoots()
+    {
+        double root1=0,root2=0;
+         double result[]=new double[2];
+         if(isQuadratic())
+       {
+        disc=calculateDisc();
+        if(disc>0)
+        {
+            root1 = (-b + Math.sqrt(disc)) / (2 * a);
+            root2 = (-b - Math.sqrt(disc)) / (2 * a);
+            result[0]=root1;
+        result[1]=root2;
+
+        }
+        else if(disc==0)
+        {
+             root1 = root2 = -b / (2 * a);
+             result[0]=root1;
+        result[1]=root2;
+        }
+        else if(disc<0)
+        {
+            double realPart = -b / (2 *a);
+            double imaginaryPart = Math.sqrt(-disc) / (2 * a);
+            result[0]=realPart;
+        result[1]=imaginaryPart;
+       
+        }
+       }
+       
+        
+        return result;
+        
+        
+    }
+    public int calculateDisc()
+    {
+        int res=MAX_VALUE;
+        if(isQuadratic())
+        {
+            res=b*b-4*a*c;
+        }
+        return res;
+    }
+    public boolean isQuadratic()
+    {
+        boolean flag=false;
+        if(a!=0)
+        {
+            flag=true;
+        }
+        return flag;
+    }
+    public boolean isQuadrati(int a,int b,int c)
+    {
+        this.a=a;
+        boolean flag=false;
+        if(a!=0)
+        {
+            flag=true;
+        }
+        return flag;
+    }
+    public void display()
+    { QuadraticEquation e=new QuadraticEquation();
+        double roots[]=new double[2];
+         roots=calculateRoots();
+       
+        if(calculateDisc()>0)
+        {
+          System.out.println("root1 = "+roots[0]);
+          System.out.println("root2 = "+roots[1]);   
+        }
+         if(calculateDisc()==0)
+        {
+          System.out.println("root1 = root2 = "+roots[0]);
+           
+        }
+          if(calculateDisc()<0)
+        {
+          System.out.println("root1 = "+roots[0]+"+"+roots[1]+"i");
+            System.out.println("root1 = "+roots[0]+"-"+roots[1]+"i");
+        }
+    }
+    public void takeInput()
+    {
+        int a,b,c;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the values of a ");
+                a=in.nextInt();
+                System.out.println("Enter the values of b ");
+                b=in.nextInt();
+                System.out.println("Enter the values of c ");
+                c=in.nextInt();
+                setValues(a,b,c);
+    }
+    public static void main(String[] args) {
+        QuadraticEquation e=new QuadraticEquation();
+        System.out.println("1) calculate Roots");
+        System.out.println("2) calculate Sum of Roots");
+        System.out.println("3) calculate Product Of Roots");
+        System.out.println("4) Check Equation is Quadratic?");
+           int choice;
+           
+           Scanner in = new Scanner(System.in);
+           choice=in.nextInt();
+        switch(choice)
+        {
+            case 1:
+            {
+                e.takeInput();
+                
+              if(e.isQuadratic())
+              {
+                e.display();
+                 
+              }
+              else
+              {
+               System.out.println("Not a Quadratic Equation");
+              }
+            }
+            case 2:
+            {
+                e.takeInput();
+                 if(e.isQuadratic())
+              {
+                  System.out.println(e.RootsSum());
+                 
+              }
+              else
+              {
+               System.out.println("Not a Quadratic Equation");
+              }
+            }
+            case 3:
+            {
+                e.takeInput();
+                 if(e.isQuadratic())
+              {
+                  System.out.println(e.RootProduct());
+                 
+              }
+              else
+              {
+               System.out.println("Not a Quadratic Equation");
+              }
+            }
+            case 4:
+            {
+                e.takeInput();
+                 if(e.isQuadratic())
+              {
+                 
+                  System.out.println("Quadratic Equation");
+                 
+              }
+              else
+              {
+               System.out.println("Not a Quadratic Equation");
+              }
+            }
+            
+        }
+    }
+    
+}
---
+++ QuadraticEquation/test/quadraticequation/QuadraticEquationIT.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadraticequation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+/**
+ *
+ * @author Nabiya Fatima
+ */
+public class QuadraticEquationIT {
+    QuadraticEquation e=new QuadraticEquation();
+     QuadraticEquation e1=new QuadraticEquation();
+      QuadraticEquation e2=new QuadraticEquation();
+       QuadraticEquation e3=new QuadraticEquation();
+       QuadraticEquation e4=new QuadraticEquation();
+      QuadraticEquation e5=new QuadraticEquation();
+       QuadraticEquation e6=new QuadraticEquation();
+      
+   
+    
+    @Before
+    public void setUp() {
+       e.setValues (1, 0, 0);
+       e1.setValues (1, 2, 1);
+       e2.setValues (1, 4, 5);
+       e3.setValues(1, 3, 2);
+       e4.setValues(0, 1, 2);
+       e5.setValues(0, 0, 0);
+       e6.setValues(0, 0, 1);
+       
+    }
+     @After
+    public void tearDown() {
+     System.out.println("Function tested Successfully");
+       
+    }
+    @RunWith(Parameterized.class)
+public class root {
+   private Integer a;
+  private Integer b;
+  private Integer c;
+  private Integer expected;
+   private QuadraticEquation q;
+
+   @Before
+   public void initialize() {
+      q=new QuadraticEquation();
+   }
+	
+   public root(int a,int b,int c,int expected) {
+      this.a=a;
+      this.b=b;
+      this.c=c;
+      this.expected=expected;
+   }
+
+   @Parameterized.Parameters
+   public  Collection values() {
+      return Arrays.asList(new Object[][] {
+         { 1,2,1,true },
+         { 1,4,5,true },
+         { 1,3,2,true }
+         
+      });
+   }
+
+   // This test will run 3 times since we have 5 parameters defined
+   
+   
+   @Test
+   public void testIsQuadratic() {
+    
+     
+     assertTrue(q.isQuadrati(a, b, c));
+   }
+}
+     
+    @Test 
+    public void TestCheckRoots()
+    {
+        String res1=e1.CheckRoots();
+        String res2=e2.CheckRoots();
+        String res3=e3.CheckRoots();
+        assertNull("Not a Quadratic Equation",e4.CheckRoots());
+       assertNull("Not a Quadratic Equation",e4.CheckRoots());
+       assertNull("Not a Quadratic Equation",e5.CheckRoots());
+       assertNotNull("Quadratic Equation",e1.CheckRoots());
+       assertNotNull("Quadratic Equation",e2.CheckRoots());
+       assertNotNull("Quadratic Equation",e3.CheckRoots());
+       assertSame("The roots are equal","equal", res1);
+         assertSame("The roots are equal","equal", e.CheckRoots());
+       assertSame("The roots are imaginary","imaginary", res2);
+       assertSame("The roots are real","real", res3);
+       assertNotSame("The roots are not equal","equal", res2);
+       assertNotSame("The roots are not imaginary","imaginary", res3);
+       assertNotSame("The roots are  not real","real", res1);
+       
+    }
+     @Test 
+    public void TestIsQuadratic()
+    {
+        boolean res1=e1.isQuadratic();
+        boolean res2=e2.isQuadratic();
+        boolean res3=e3.isQuadratic();
+      assertTrue("Quadratic Equation",res1);
+      assertTrue("Quadratic Equation",res1);
+      assertTrue("Quadratic Equation",res1);
+       assertFalse("Not a Quadratic Equation",e4.isQuadratic());
+      assertFalse("Not a Quadratic Equation",e5.isQuadratic());
+      assertFalse("Not a Quadratic Equation",e6.isQuadratic());
+      
+    }
+    
+    @Test
+    public void TestRootsSum()
+    {
+        
+        assertEquals(-2,e1.RootsSum(),0.0000);
+        assertEquals(-4.0,e2.RootsSum(),0.0000);
+        assertEquals(-3.0,e3.RootsSum(),0.0000);
+        assertNotEquals(-2,e4.RootsSum(),0.0000);
+        assertNotEquals(-4.0,e5.RootsSum(),0.0000);
+        assertNotEquals(-3.0,e6.RootsSum(),0.0000);
+    }
+   
+    
+    @Test
+    public void TestRootProduct()
+    {
+        
+        assertEquals(1,e1.RootProduct());
+        assertEquals(5,e2.RootProduct());
+        assertEquals(2,e3.RootProduct());
+        assertNotEquals(-2,e4.RootProduct());
+        assertNotEquals(-4.0,e5.RootProduct());
+        assertNotEquals(-3.0,e6.RootProduct());
+    }
+  
+    
+    @Test
+    public void TestCalculateRoots()
+    {
+        double []expected=new double[2];
+          expected[0]=expected[1]=-1;  
+        double res[]=e1.calculateRoots();
+        assertArrayEquals("Equal roots",expected,res,0.0000);
+      
+         expected[0]=-2;expected[1]=1; 
+         res=e2.calculateRoots();
+        assertArrayEquals("Imaginary roots",expected,res,0.0000);
+        
+        expected[0]=-1;expected[1]=-2; 
+         res=e3.calculateRoots();
+        assertArrayEquals("Real roots",expected,res,0.0000);
+    }
+    
+    
+     @RepeatedTest(5)
+    void repeatedTestWithRepetitionInfo(RepetitionInfo repetitionInfo) {
+        assertEquals(5, repetitionInfo.getTotalRepetitions());
+        e3.setValues(-2, 7, 4);
+        
+         double []expected=new double[2];
+          expected[0]=expected[1]=-1;  
+        double res[]=e1.calculateRoots();
+        assertArrayEquals("Equal roots",expected,res,0.0000);
+      
+         expected[0]=-2;expected[1]=1; 
+         res=e2.calculateRoots();
+        assertArrayEquals("Imaginary roots",expected,res,0.0000);
+        
+        expected[0]=-4;expected[1]=-0.5; 
+         res=e3.calculateRoots();
+        assertArrayEquals("Real roots",expected,res,0.0000);
+    }
+   
+ 
+
+}
